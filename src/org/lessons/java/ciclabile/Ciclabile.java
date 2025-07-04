894cf98f74d3b5c3d8ed799ec64f190d7a26c095
MarcoVacchi <mvacchi816@gmail.com>
2025-07-04 14:36:13 +0200

    first commit

---
+++ src/org/lessons/java/ciclabile/Ciclabile.java
@@ -0,0 +1,49 @@
+package org.lessons.java.ciclabile;
+
+public class Ciclabile {
+
+    public int[] arrayInt = {};
+    public int currentIndex = 0;
+
+    public Ciclabile(int[] arrayInt) {
+        this.arrayInt = arrayInt;
+
+    }
+
+    public int getElementoSuccessivo() {
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (currentIndex < arrayInt.length) {
+                currentIndex++;
+                return arrayInt[currentIndex - 1];
+            }
+        }
+        return -1;
+
+    };
+
+}
+
+// Vogliamo realizzare una classe che contenga un elenco di interi e che ci
+// permetta di ciclarli (dal primo all’ultimo).
+
+// Dovrà offrire due metodi :
+// - int getElementoSuccessivo() (che restituisce il prossimo elemento rispetto
+// all’ultima volta che è stato invocato. Cioè la prima volta restituisce il
+// primo elemento, la seconda volta il secondo, …)
+// - boolean hasAncoraElementi() (che deve restituire true se ci sono ancora
+// elementi da restituire)
+
+// La classe deve avere un costruttore che prende come parametro un array di
+// interi (che sarà l’elenco che viene gestito internamente)
+
+// Internamente alla classe vogliamo mantenere l’elenco di interi come array, no
+// ArrayList o simili.
+
+// Bonus :
+// prevedere anche un costruttore che non prenda parametri e un metodo
+// addElemento che permetta di aggiungere un nuovo intero all’elenco da ciclare.
+
+// Nota bene:
+// Vanno utilizzati solo gli array nativi e quindi non è possibile usare
+// iterator vari.
---
+++ src/org/lessons/java/ciclabile/Main.java
@@ -0,0 +1,17 @@
+package org.lessons.java.ciclabile;
+
+public class Main {
+    public static void main(String[] args) {
+        Ciclabile ciclabile = new Ciclabile(new int[] { 3, 2, 5, 21, 344 });
+        System.out.println(ciclabile.getElementoSuccessivo());
+        System.out.println(ciclabile.getElementoSuccessivo());
+        System.out.println(ciclabile.getElementoSuccessivo());
+        System.out.println(ciclabile.getElementoSuccessivo());
+        System.out.println(ciclabile.getElementoSuccessivo());
+        System.out.println(ciclabile.getElementoSuccessivo());
+        System.out.println(ciclabile.getElementoSuccessivo());
+        System.out.println(ciclabile.getElementoSuccessivo());
+        System.out.println(ciclabile.getElementoSuccessivo());
+    }
+
+}
